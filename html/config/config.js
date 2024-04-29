3b9792606b55ade340f3ad9cbb6c9634a9f80e19
Jaehyuk-Lee <alex00728@outlook.com>
2024-04-29 17:02:14 +0900

    add admin function

--- html/config/config.js
+++ html/config/config.js
@@ -1,1 +1,28 @@
 const WAS_IP = '192.168.111.21';
+
+const TYPE_INFO = {
+  search: {
+    params: [
+      'name',
+    ],
+    method: 'GET',
+  },
+  searchUserID: {
+    params: [
+      'userID',
+    ],
+    method: 'GET',
+  },
+  update: {
+    params:
+      [
+        'name',
+        'residentRegistrationNumber',
+        'contactNumber',
+        'address',
+        'email',
+        'userID',
+      ],
+    method: 'POST',
+  }
+};
--- html/scripts.js
+++ html/scripts.js
@@ -1,22 +1,49 @@
 function send(type) {
   const SERVER_IP = `http://${WAS_IP}:3000/${type}/`;
-  let name = document.querySelector("input[name='name']").value;
-  sendGet(SERVER_IP, name);
+  let texts = document.querySelectorAll("div#input input[type='text']");
+  let numbers = document.querySelectorAll("div#input input[type='number']");
+  let selects = document.querySelectorAll("div#input select");
+  let params = {};
+  [texts, numbers, selects].map(input => input.forEach(key => {
+    if (TYPE_INFO[type].params.indexOf(key.name) != -1)
+      params[key.name] = key.value;
+  }));
+  httpRequest(
+    SERVER_IP,
+    TYPE_INFO[type].method,
+    type,
+    params
+  );
 }
 
-function sendGet(url, name = '') {
+function httpRequest(url, method, type, params) {
   var http = new XMLHttpRequest();
-  http.onreadystatechange = () => {
-    if (http.readyState === XMLHttpRequest.DONE) {
-      if (http.status === 200) {
-        var result = http.response;
-        alert(result);
-      } else {
-        alert('뭔가 문제가 있어요.\nHTTP Status Code: ' + http.status);
+  var param = JSON.stringify({ params });
+  console.log(param);
+  http.open("GET", url + "?param=" + param, true);
+  http.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+  http.onreadystatechange = function () {
+    if (http.status == 200) {
+      if (http.readyState == 4) {
+        const res = JSON.parse(http.responseText);
+        if (type === 'update' && res.affectedRows) {
+          alert('성공적으로 처리되었습니다.\n영향을 받은 행 수: ' + res.affectedRows);
+        }
+        else if (type === 'searchUserID') {
+          console.log(res[0]);
+          for (const [key, value] of Object.entries(res[0])) {
+            let k = key[0].toLowerCase() + key.slice(1);
+            document.querySelector(`input[name='${k}']`).value = value;
+          }
+        }
+        else if (type === 'search') {
+          alert(res);
+        }
       }
+    } else {
+      alert('뭔가 문제가 있어요.\nHTTP Status Code: ' + http.status);
     }
   }
-  http.open("GET", url + "?name=" + name);
-  http.setRequestHeader("Content-type", "multipart/form-data");
   http.send();
 }
